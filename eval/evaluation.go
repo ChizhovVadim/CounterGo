2c2276c79fb06a2524775b26d8fa2668a2465683
Vadim Chizhov <chizhov.vadim@gmail.com>
2019-05-20 14:38:42 +0300

    Eval: K+R VS K

--- eval/evaluation.go
+++ eval/evaluation.go
@@ -41,7 +41,7 @@ func (e *EvaluationService) initPst() {
 	var (
 		knightLine = [8]int{0, 2, 3, 4, 4, 3, 2, 0}
 		bishopLine = [8]int{0, 1, 2, 3, 3, 2, 1, 0}
-		kingLine   = [8]int{0, 2, 3, 4, 4, 3, 2, 0}
+		kingLine   = [8]int{0, 3, 5, 6, 6, 5, 3, 0}
 	)
 	for sq := 0; sq < 64; sq++ {
 		var f = File(sq)

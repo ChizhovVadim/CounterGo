63c487ca724c620f71c129d62129c6fb9109c872
Vadim Chizhov <chizhov.vadim@gmail.com>
2024-01-12 09:26:39 +0300

    update build script

--- build.sh
+++ build.sh
@@ -9,10 +9,11 @@ function make {
         GOOS=$1 GOARCH=$2 go build \
                 -ldflags "-X 'main.gitRevision=$gitRevision' -X 'main.buildDate=$buildDate' -X 'main.versionName=$versionName'" \
                 -o counter-$versionName-$1-$2$3 \
+                -tags=avx,embed \
                 ./cmd/counter
 }
 
-make  darwin amd64 ""
-make  darwin arm64 ""
+#make  darwin amd64 ""
+#make  darwin arm64 ""
 make   linux amd64 ""
 make windows amd64 ".exe"
